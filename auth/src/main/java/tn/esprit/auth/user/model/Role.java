a15955893b6d29fa33c64282495f74ed8790aec3
anaamane <ahmed.naamane@example.com>
2021-06-20 16:18:42 +0100

    authentification+authorization

--- auth/src/main/java/tn/esprit/auth/user/model/Role.java
+++ auth/src/main/java/tn/esprit/auth/user/model/Role.java
@@ -1,50 +1,47 @@
-package tn.esprit.auth.user.model;                                        
- 
-import java.util.Set;
- 
+package tn.esprit.auth.user.model;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
- 
-import org.springframework.security.core.GrantedAuthority;
- 
-@Entity
-public class Role  implements GrantedAuthority{
- 
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long id;
-        private String name;
-        
-        @ManyToMany(mappedBy = "roles")
-        private Set<User> users;
-        
-        
-        
-        public String getName() {
-                return name;
-        }
-        public void setName(String name) {
-                this.name = name;
-        }
-        public Long getId() {
-                return id;
-        }
-        public void setId(Long id) {
-            this.id = id;
-    }
-    public Set<User> getUsers() {
-            return users;
-    }
-    public void setUsers(Set<User> users) {
-            this.users = users;
-    }
-    @Override
-    public String getAuthority() {
-            // TODO Auto-generated method stub
-            return name;
-    }
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+	public Role() {
+
+	}
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
 }
--- auth/src/main/java/tn/esprit/auth/user/model/User.java
+++ auth/src/main/java/tn/esprit/auth/user/model/User.java
@@ -1,109 +1,134 @@
 package tn.esprit.auth.user.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @Entity
+@Table(	name = "users", 
+		uniqueConstraints = { 
+			@UniqueConstraint(columnNames = "username"),
+			@UniqueConstraint(columnNames = "email") 
+		})
 public class User {
+	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank
+	@Size(max = 20)
+	private String username;
 	
-	
+	private String firstName;
+	private String lastName;
 
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
 
+	@NotBlank
+	@Size(max = 120)
+	private String password;
 
-	@JoinColumn
-    @OneToOne(cascade = CascadeType.ALL)
-    private UserDetailsImpl userDetails;
-	
-	@ManyToMany
-	@JoinTable(name="user_role",joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns = @JoinColumn(name="role_id"))
-	private Set<Role> roles;
-	
-	
-    public User() {
-        
-    }
-    
-    
-    public User(UserDetailsImpl userDetails) {
-        this.userDetails = userDetails;
-}
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(	name = "user_roles", 
+				joinColumns = @JoinColumn(name = "user_id"), 
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
-    public User(Long id, UserDetailsImpl userDetails, Set<Role> roles) {
-		super();
-		this.id = id;
-		this.userDetails = userDetails;
-		this.roles = roles;
-	}
-    
-    
-
-
-	public User(UserDetailsImpl userDetails, Set<Role> roles) {
-		super();
-		this.userDetails = userDetails;
-		this.roles = roles;
+	public User() {
 	}
 
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+	
+
+	public User(@NotBlank @Size(max = 20) String username, 
+			@NotBlank @Size(max = 50) @Email String email, String firstName, String lastName,@NotBlank @Size(max = 120) String password) {
+		super();
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-    
-    
 
-/*
-	public long getId() {
-	        return id;
-	}*/
-
-
-
-	public UserDetailsImpl getUserDetails() {
-	        return userDetails;
-	}
-	
-	public void setUserDetails(UserDetailsImpl userDetails) {
-	        this.userDetails = userDetails;
+	public String getUsername() {
+		return username;
 	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Set<Role> getRoles() {
 		return roles;
 	}
 
-
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
+	public String getLastName() {
+		return lastName;
+	}
 
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-
-
-	
-	
-	
-	
-	
 }
