96ce53ec1601ded8acdc633b3e6ee8736487043f
amd <ahmed.naamane@example.com>
2022-03-19 15:00:48 +0100

    dockerfile+eureka

--- auth/src/main/java/tn/esprit/auth/AuthApplication.java
+++ auth/src/main/java/tn/esprit/auth/AuthApplication.java
@@ -2,8 +2,11 @@ package tn.esprit.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
