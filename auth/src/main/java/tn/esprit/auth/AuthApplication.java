adaedc21dac39e53e7d02fb5ea9c85a6612e82d6
DoniaAzib <d.azib@avidea.tn>
2022-03-20 19:31:54 +0100

    add eureka client

--- auth/src/main/java/tn/esprit/auth/AuthApplication.java
+++ auth/src/main/java/tn/esprit/auth/AuthApplication.java
@@ -2,12 +2,10 @@ package tn.esprit.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"tn.esprit.auth.entity","tn.esprit.auth.user.model"})
-@EnableJpaRepositories({"tn.esprit.auth.repository","tn.esprit.auth.user.repository"})
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
