6584547c429cb1667c834eaf7c661a4eaf14e15d
ines.abbouz <ines.abbouz@iliadeconsulting.com>
2022-03-20 20:59:18 +0100

    branch_ines: eureka configuration in commandeApp

--- auth/src/main/java/tn/esprit/auth/AuthApplication.java
+++ auth/src/main/java/tn/esprit/auth/AuthApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"tn.esprit.auth.entity","tn.esprit.auth.user.model"})
 @EnableJpaRepositories({"tn.esprit.auth.repository","tn.esprit.auth.user.repository"})
+@EnableEurekaClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
